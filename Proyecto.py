1a30055f898a7ff54153324a341a189faa6a9598
Doxopat <patrickzferruscaaguilera@gmail.com>
2023-03-03 15:57:40 -0600

    ENLISTAR PRODUCTOS

--- Proyecto.py
+++ Proyecto.py
@@ -1,3 +1,20 @@
+dic = {110: 'CocaCola', 111:'Pepsi', 120: 'Red cola', 121: 'Monster',
+        130: 'Fanta', 131: 'Sprite', 140: 'Peñafiel', 141: 'Jarrito',
+        210: 'Takis', 211: 'Doritos', 220: 'Cheetos', 221: 'Chips', 
+        230: 'Sabritas', 231: 'Ruffles', 240: 'Runners', 241: 'Toreadas',
+        511: 'Chokis', 610: 'Emperador', 611:'Principe', 620: 'Marias',
+        621: 'Deliciosas', 630: 'Arcoiris', 631: 'Quaker'}
+
+
+dicGa = {511: 'Chokis', 610: 'Emperador', 611:'Principe', 620: 'Marias',
+        621: 'Deliciosas', 630: 'Arcoiris', 631: 'Quaker'}
+
+dicPapas = {210: 'Takis', 211: 'Doritos', 220: 'Cheetos', 221: 'Chips', 
+        230: 'Sabritas', 231: 'Ruffles', 240: 'Runners', 241: 'Toreadas'}
+
+dicSodas = {110: 'CocaCola', 111:'Pepsi', 120: 'Red cola', 121: 'Monster',
+        130: 'Fanta', 131: 'Sprite', 140: 'Peñafiel', 141: 'Jarrito'}
+
 class MaquinaExpendedora:
     def __init__(self, productos: dict, dinero: float) -> None:
         self.productos = productos
@@ -7,29 +24,99 @@ class MaquinaExpendedora:
     def EntregarProducto(Codigo_llave: int, Dic_Productos: dict)-> str:
         return Dic_Productos.get(Codigo_llave)
         
-    def SeleccionarProducto()->None:
-        print("Aqui se imprime la lista de valores del diccionario")
+    def EnlistarProducto():
+        print('\n-----Productos totales-----\n')
+        print(dic)
+        print('\n-----Galletas Disponibles----\n')
+        print(dicGa)
+        print('\n-----Papas Disponibles-----\n')
+        print(dicPapas)
+        print('\n-----Sodas Disponibles-----\n')
+        print(dicSodas)
+
     
+class User:
+    def __init__(self, nombre):
+        self.nombre=nombre
+
+class Usuario(User):
+    def __init__(self):
+        User.__init__(self,'Usuario')
+    def SeleccionarProducto(self):
+        pass
+class UsuarioPremium(User):
+    def __init__ (self,  usuario, contraseña, edad, compañia):
+        User.__init__(self, self.nombre)
+        self.usuario=usuario
+        self.contraseña=contraseña
+        self.edad=edad
+        self.compañia=compañia
+
+
 
 class Producto:
     def __init__(self, nombre, precio, codigo):
         self.nombre = nombre
         self.precio = precio
         self.codigo = codigo
+
+    # def IniciarProductos():
+    #     dic = {110: 'CocaCola', 111:'Pepsi', 120: 'Red cola', 121: 'Monster',
+    #     130: 'Fanta', 131: 'Sprite', 140: 'Peñafiel', 141: 'Jarrito',
+    #     210: 'Takis', 211: 'Doritos', 220: 'Cheetos', 221: 'Chips', 
+    #     230: 'Sabritas', 231: 'Ruffles', 240: 'Runners', 241: 'Toreadas',
+    #     511: 'Chokis', 610: 'Emperador', 611:'Principe', 620: 'Marias',
+    #     621: 'Deliciosas', 630: 'Arcoiris', 631: 'Quaker'}#Seguir agregando productos al diccionario
+
     
 
-def IniciarProductos():
-    dict = {0110: 'CocaCola', 0111:'Pepsi', 0210: 'Donas'}#Seguir agregando productos al diccionario
+class Sodas(Producto):
     
+    def agregar():
+        pass
+
+    def eliminar():
+        pass
+
+    def imprimir():
+        print(dicGa)
     
+
+class Papas(Producto):
+
+    def agregar():
+        pass
+
+    def eliminar():
+        pass
+
+    def imprimir():
+        print(dicGa)
     
     
 
-# 1. Entregar producto.
-# 2. Seleccionar producto.
-# 3. Almacenar nuevo producto.
-# 4. Enlistar total de productos de un tipo.
-# 5. Enlistar todos los productos.
-# 6. Mostrar costos.
-# 7. Regresar cambio.
-# 8. Manejo de efectivo (mostrar total, alertar por falta de efectivo, etc.)
+class Galletas(Producto):
+
+    def agregar():
+        pass
+
+    def eliminar():
+        pass
+
+    def imprimir():
+        print(dicGa)
+    
+
+def main():
+
+    print('\n-----Bienvenido a TuMaquinaExpendedora-----\n')
+    nombre = str(input('Dinos tu nombre: '))
+
+    print('Bienvenido' + nombre + 'Seleccione la opcion que desee...\n'
+          + '1. Comprar' + '2. Salir')
+    opc = int(input('Opcion: '))
+    
+
+    MaquinaExpendedora.EnlistarProducto()
+
+
